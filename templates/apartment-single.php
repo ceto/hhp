6381b0236d6344316bd1bad8506c9e6a94bf0c66
Gabor Szabo <szabogabi@gmail.com>
2015-03-26 17:43:06 +0100

    kisdeploy

--- templates/apartment-single.php
+++ templates/apartment-single.php
@@ -3,7 +3,7 @@
   </div>
 </header>
 <div class="wrapper wrapper--fullwidth wrapper-apartment ">
-  <div class="page-header">
+  <div class="page-header inverse">
     <?php previous_post_link('%link','<i class="ion ion-ios7-arrow-left"></i>'); ?>
     <h2><span>Finn Din Bolig</span> <?php the_title(); ?></h2>
     <?php next_post_link('%link','<i class="ion ion-ios7-arrow-right"></i>'); ?>
@@ -11,12 +11,24 @@
   <div class="apartment-details is_light">
     <div class="apartment-details--left">
       <div class="apartment-details--left--inner">
-        <figure class="apartment-3dfloormap">
-          <?php $leftimgsrc = wp_get_attachment_image_src( get_post_meta($post->ID, '_meta_floormap_id', true), '640free');  ?>
-          <a class="popup-zoom" href="<?php echo get_post_meta($post->ID, '_meta_floormap', true); ?>">
-            <img src="<?php echo $leftimgsrc[0] ?>" />
-          </a>
-        </figure>
+
+          <figure class="apartment-schema">
+            <?php $schemaimgsrc = wp_get_attachment_image_src( get_post_meta($post->ID, '_meta_schema_id', true), '480free');  ?>
+            <a class="popup-zoom" href="<?php echo get_post_meta($post->ID, '_meta_schema', true); ?>">
+              <img src="<?php echo $schemaimgsrc[0]; ?>" />
+            </a>
+          </figure>
+
+          <figure class="apartment-2dfloormap">
+            <?php $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'orig');  ?>
+            <a class="popup-zoom" href="<?php echo $imgsrc[0]; ?>">
+              <?php the_post_thumbnail('480free'); ?>
+            </a>
+          </figure>
+
+
+
+
       </div>
     </div>
 
@@ -26,18 +38,17 @@
       
       <div class="apartment-details--right--inner-white">
         <div class="holdit nohorizpadding">
-          <figure class="apartment-2dfloormap">
-            <?php $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'orig');  ?>
-            <a class="popup-zoom" href="<?php echo $imgsrc[0]; ?>">
-              <?php the_post_thumbnail('480free'); ?>
-            </a>
-          </figure>
-          <figure class="apartment-schema">
-            <?php $schemaimgsrc = wp_get_attachment_image_src( get_post_meta($post->ID, '_meta_schema_id', true), '480free');  ?>
-            <a class="popup-zoom" href="<?php echo get_post_meta($post->ID, '_meta_schema', true); ?>">
-              <img src="<?php echo $schemaimgsrc[0]; ?>" />
-            </a>
-          </figure>
+        
+        <figure class="apartment-3dfloormap">
+          <?php $leftimgsrc = wp_get_attachment_image_src( get_post_meta($post->ID, '_meta_floormap_id', true), '640free');  ?>
+          <a class="popup-zoom" href="<?php echo get_post_meta($post->ID, '_meta_floormap', true); ?>">
+            <img src="<?php echo $leftimgsrc[0] ?>" />
+          </a>
+        </figure>
+
+
+
+
         </div>
       </div>
 
@@ -68,13 +79,12 @@
       <div class="apartment-details--right--inner-red">
         <div class="holdit">
 
-
           <div class="apartment-facts">
           <?php if ( get_post_meta( $post->ID, '_meta_3dpano', true )) : ?>
-            <a class="popup-3d apartment-3dlink" href="<?php echo get_post_meta( $post->ID, '_meta_3dpano', true ); ?>">
+<!--             <a class="popup-3d apartment-3dlink" href="<?php echo get_post_meta( $post->ID, '_meta_3dpano', true ); ?>">
               <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/360.png" alt="View 3D"><br>
              Klikk for å se 3D 
-            </a>
+            </a> -->
           <?php endif; ?>
             <p class="data-item"><span>Navn</span> <?php the_title(); ?></p>
             <p class="data-item"><span>Etasje</span> <?php echo get_post_meta( $post->ID, '_meta_floor', true ); ?></p>
@@ -85,13 +95,13 @@
             <?php if (get_post_meta( $post->ID, '_meta_state', true )!=='utsolgt'): ?>
             <p class="price data-item"><span>Pris</span> <?php echo number_format(get_post_meta( $post->ID, '_meta_pris', true ), 0, ',', ' '); ?> NOK</p>
             <?php endif; ?>
-            <p class="data-item"><span>Type <?php echo $type->name; ?></span> <?php echo $samelist; ?></p>
+            <!-- <p class="data-item"><span>Type <?php echo $type->name; ?></span> <?php echo $samelist; ?></p> -->
             <p class="data-item"><span>Status</span> <?php echo st_conv(get_post_meta( $post->ID, '_meta_state', true )); ?></p>
           </div>
 
           <div class="apartment-discl">
             <?php the_content(); ?>
-            <a class="btn btn-light" data-toggle="collapse" data-target="#contactblock">Meld din interesse</a>
+            <a class="btn btn-light" data-toggle="collapse" data-target="#contactblock">Meld din interesse</a>           <a class="btn" href="<?php echo get_post_meta( $post->ID, '_meta_pdf', true ); ?>">Last ned PDF <i class="ion ion-clipboard"></i></a>
           </div>
         </div>
       </div>
--- templates/chooser-start.php
+++ templates/chooser-start.php
@@ -8,10 +8,10 @@
 ?>
 <div class="thechooser">
 	<div id="visual-chooser" class="visual-chooser visual-chooser-starter">
-		<a class="popup-3d chooser-3dlink" href="<?php echo esc_url(home_url('/')); ?>wp-content/uploads/3dpanoramas/ext/index.html">
+<!-- 		<a class="popup-3d chooser-3dlink" href="<?php echo esc_url(home_url('/')); ?>wp-content/uploads/3dpanoramas/ext/index.html">
 			<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/360.png" alt="View 3D"><br>
 			Klikk for å se 3D 
-		</a>
+		</a> -->
 	<a class="btn btn-light" data-toggle="collapse" data-target="#detailswrapper">Leilighetsoversikt <i class="ion ion-plus"></i></a>
 	</div>
 	<div id="detailswrapper" class="wrapper wrapper--fullwidth detailswrapper collapse">
